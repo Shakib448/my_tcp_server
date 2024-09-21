01ec9958629922ad0696654164f07fe0c0e25d90
Shakib448 <shakiba448@gmail.com>
2024-09-21 22:26:37 +0600

    WIP on tcp server added javascript image etc

--- html/app.js
+++ html/app.js
@@ -3,3 +3,5 @@ const myButton = document.getElementById("myButton");
 myButton.addEventListener("click", () => {
   alert("Button clicked with addEventListener!");
 });
+
+console.log("Hello rust you are awesome");
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening on 127.0.0.1:8080");
 
     for steam in listener.incoming() {
         let stream: TcpStream = steam.unwrap();
@@ -17,19 +18,39 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let index = String::from("./html/index.html");
-    let four_o_four = String::from("./html/404.html");
+    let base_path = String::from("./html");
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", Path::new(&index))
+        ("HTTP/1.1 200 OK", format!("{}/index.html", base_path))
     } else {
-        ("HTTP/1.1 404 NOT FOUND", Path::new(&four_o_four))
+        let requested_file = &request_line[4..request_line.find("HTTP/1.1").unwrap() - 1];
+        let full_path = format!("{}{}", base_path, requested_file);
+        if Path::new(&full_path).exists() {
+            ("HTTP/1.1 200 OK", full_path)
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", format!("{}/404.html", base_path))
+        }
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let content_type = if filename.ends_with(".html") {
+        "text/html"
+    } else if filename.ends_with(".js") {
+        "application/javascript"
+    } else if filename.ends_with(".css") {
+        "text/css"
+    } else {
+        "application/octet-stream"
+    };
+
+    println!("{filename:?} and {content_type:?}");
+
+    let contents = fs::read(&filename).unwrap_or_else(|_| Vec::new());
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\nContent-Type: {content_type}\r\n\r\n"
+    );
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(&contents).unwrap();
 }
