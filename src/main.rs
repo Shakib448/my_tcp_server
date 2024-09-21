a6b7de1575220d032b535a12c87a8a1d35cdf61c
Shakib448 <shakiba448@gmail.com>
2024-09-21 21:59:46 +0600

    WIP on tcp server

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    path::Path,
 };
 
 fn main() {
@@ -13,17 +14,19 @@ fn main() {
         handle_connection(stream);
     }
 }
-#[warn(unused_variables)]
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let index = String::from("./html/index.html");
+    let four_o_four = String::from("./html/404.html");
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("play.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", Path::new(&index))
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", Path::new(&four_o_four))
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
